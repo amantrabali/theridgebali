e43596c4e4cc50348dbff2ef79b1145323ad2f3e
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-16 01:33:34 +0800

    Update archive.php

--- archive.php
+++ archive.php
@@ -9,18 +9,17 @@
 
 get_header();
 ?>
+
 	<main id="primary" class="site-main">
+
 		<?php if ( have_posts() ) : ?>
+
 			<header class="page-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
-			<!-- Blog Grid Start -->
-			<div class="container-fluid blog-section">
-		    <div class="row justify-content-center">
 
 			<?php
 			/* Start the Loop */
@@ -32,20 +31,21 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content-archive', get_post_type() );
+				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
-			?>
 
-			</div>
-			</div>
-
-			<?php
+			the_posts_navigation();
 
 		else :
+
 			get_template_part( 'template-parts/content', 'none' );
+
 		endif;
-		?>		
+		?>
+
 	</main><!-- #main -->
+
 <?php
+get_sidebar();
 get_footer();
