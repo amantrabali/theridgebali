f6bba06ce3c0f7d5f6f46fca6d6c03da9e0564a6
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-16 00:03:42 +0800

    Update archive.php

--- archive.php
+++ archive.php
@@ -9,12 +9,8 @@
 
 get_header();
 ?>
-
 	<main id="primary" class="site-main">
-
-
 		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
@@ -46,17 +42,10 @@ get_header();
 
 			<?php
 
-			the_posts_navigation();
-
 		else :
-
 			get_template_part( 'template-parts/content', 'none' );
-
 		endif;
-		?>
-		
+		?>		
 	</main><!-- #main -->
-
 <?php
-get_sidebar();
 get_footer();
